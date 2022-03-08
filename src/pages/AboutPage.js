483a7d689ed90d9ffc24a880873a114d711ecf36
faith okpala <okpalafaith1@gmail.com>
2022-03-08 14:52:09 +0100

    about page done

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -4,7 +4,26 @@ import { PageHero } from '../components'
 import aboutImg from '../assets/hero-bcg.jpeg'
 
 const AboutPage = () => {
-  return <h4>about page</h4>
+  return <main>
+    <PageHero title= 'about'/>
+  <Wrapper className='page section section-center'>
+    <img src={aboutImg} alt='Nice desk' />
+    <article>
+          <div className='title'>
+            <h2>our story</h2>
+            <div className='underline'></div>
+          </div>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam
+            quaerat, modi doloremque necessitatibus eum dolor nesciunt delectus,
+            voluptate blanditiis, obcaecati beatae ab aut ipsa consequuntur
+            tempora cumque. Ut quo enim vero odio minus nostrum eveniet,
+            doloribus veritatis dolorem unde ipsum, voluptatibus totam.
+            Explicabo, quas libero! Laborum incidunt minima consequatur ratione?
+          </p>
+        </article>
+  </Wrapper>
+  </main>
 }
 
 const Wrapper = styled.section`
