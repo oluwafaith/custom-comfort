4c764471eab1a3e3ad0dc61bf5d4a72503a65a9f
faith okpala <okpalafaith1@gmail.com>
2022-03-08 16:28:58 +0100

    worked on the home page

--- src/context/products_context.js
+++ src/context/products_context.js
@@ -15,6 +15,13 @@ import {
 
 const initialState = {
   isSidebarOpen: false,
+  products_loading: false,
+  products_error: false,
+  products: [],
+  featured_products: [],
+  single_product_loading: false,
+  single_product_error: false,
+  single_product: {},
 }
 
 const ProductsContext = React.createContext()
@@ -29,8 +36,33 @@ export const ProductsProvider = ({ children }) => {
     dispatch({type: SIDEBAR_CLOSE}) 
   } 
 
- const fetch
+ const fetchProducts = async(url) =>{
+   dispatch({type: GET_PRODUCTS_BEGIN})
 
+   try {
+     const response  = await axios.get(url)
+     const products = response.data
+     dispatch({type: GET_PRODUCTS_SUCCESS, payload: products})
+   } catch (error) {
+     dispatch(GET_PRODUCTS_ERROR)
+   }
+ }
+ 
+ const fetchSingleProduct = async (url) => {
+  dispatch({ type: GET_SINGLE_PRODUCT_BEGIN })
+  try {
+    const response = await axios.get(url)
+    const singleProduct = response.data
+    dispatch({ type: GET_SINGLE_PRODUCT_SUCCESS, payload: singleProduct })
+  } catch (error) {
+    dispatch({ type: GET_SINGLE_PRODUCT_ERROR })
+  }
+}
+
+
+  useEffect(()=>{
+    fetchProducts(url)
+  },[])
   return (
     <ProductsContext.Provider value={{...state, openSidebar, closeSidebar}}>
       {children}
