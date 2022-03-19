dbd5c2c7c2998e37af51f840f1d83202fc803423
faith okpala <okpalafaith1@gmail.com>
2022-03-19 04:31:27 +0100

    worked on cart and auth

--- src/index.js
+++ src/index.js
@@ -9,10 +9,20 @@ import { UserProvider } from './context/user_context'
 import { Auth0Provider } from '@auth0/auth0-react'
 
 ReactDOM.render(
-<ProductsProvider>
+<Auth0Provider
+    domain={process.env.REACT_APP_AUTH_DOMAIN}
+    clientId={process.env.REACT_APP_AUTH_CLIENT_ID}
+    redirectUri={window.location.origin}
+    cacheLocation='localstorage'
+  >
+    <UserProvider>
+      <ProductsProvider>
         <FilterProvider>
-
-        <App />
+          <CartProvider>
+            <App />
+          </CartProvider>
         </FilterProvider>
-</ProductsProvider>
+      </ProductsProvider>
+    </UserProvider>
+  </Auth0Provider>
 , document.getElementById('root'))
